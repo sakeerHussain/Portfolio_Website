0d752d8aca488d1f918183ab0a00359f3286779d
unknown <sakeerhussain@gmail.com>
2024-12-09 12:46:13 +0530

    third commit

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
+
 import SubmitBtn from "./submit-btn";
 import toast from "react-hot-toast";
+import { sendEmail } from "@/action/sendEmail";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
+
+
 
   return (
     <motion.section
