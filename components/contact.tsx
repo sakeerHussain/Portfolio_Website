d1f6f55abc7051d5e18301487bd895cdbc055ff7
unknown <sakeerhussain@gmail.com>
2024-12-09 18:28:29 +0530

    fifth commit

--- components/contact.tsx
+++ components/contact.tsx
@@ -35,9 +35,9 @@ export default function Contact() {
       <SectionHeading>Contact me</SectionHeading>
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
+        Contact me directly at{" "}
         <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        sakeerhussain.s123@gmail.com
         </a>{" "}
         or through this form.
       </p>
--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,8 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import Typewriter from "typewriter-effect";
+import profile from "@/public/Profile.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,13 +33,13 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              alt="Ricardo portrait"
+              src={profile}
+              alt="Sakeer portrait"
               width="192"
               height="192"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl transition-all duration-300 ease-in-out transform hover:scale-125"
             />
           </motion.div>
 
@@ -62,11 +64,26 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        Hello, I'm{" "}
+        <span className="font-bold text-blue-900 relative">
+          Sakeer Hussain. 
+        </span>
+        <span className="font-bold">
+          <Typewriter
+            options={{
+              strings: [
+                'A passionate <span style="color:#002080;">Full Stack Developer</span>,',
+                "with expertise in the MERN Stack.",
+                "With strong knowledge in",
+                "JavaScript, React, Node.js and Mongo DB",
+              ],
+              autoStart: true,
+              loop: true,
+              delay: 75,
+              deleteSpeed: 40,
+            }}
+          />
+        </span>
       </motion.h1>
 
       <motion.div
@@ -91,7 +108,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Sakeer-Resume_fullstack.pdf"
           download
         >
           Download CV{" "}
@@ -100,7 +117,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/sakeer-hussain-b8b12b280/"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +125,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/sakeerHussain"
           target="_blank"
         >
           <FaGithubSquare />
